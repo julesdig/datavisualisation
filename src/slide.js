107cfe4ab12ea64bb72d789dd73afb8a9027de33
Digonnet jules <114817002+julesdig@users.noreply.github.com>
2024-01-17 10:25:56 +0100

    update html and add details (#3)

--- src/slide.js
+++ src/slide.js
@@ -1,12 +1,39 @@
 let currentGraph = 0;
 const graphs = document.querySelectorAll('.graph-container');
 const numGraphs = graphs.length;
+let intervalId;
 
-function showNextGraph() {
+function showGraph(index) {
     graphs[currentGraph].style.display = 'none';
-    currentGraph = (currentGraph + 1) % numGraphs;
+    currentGraph = (index + numGraphs) % numGraphs;
     graphs[currentGraph].style.display = 'block';
 }
 
-setInterval(showNextGraph, 5000);
-showNextGraph();
+function showPreviousGraph() {
+    showGraph(currentGraph - 1);
+}
+
+function showNextGraph() {
+    showGraph(currentGraph + 1);
+}
+
+function togglePauseResume() {
+    const button = document.getElementById('pauseResumeButton');
+
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+        button.textContent = 'Start';
+    } else {
+        intervalId = setInterval(showNextGraph, 5000);
+        showNextGraph();
+        button.textContent = 'Pause';
+    }
+}
+
+// événements des boutons
+document.getElementById('previousButton').addEventListener('click', showPreviousGraph);
+document.getElementById('pauseResumeButton').addEventListener('click', togglePauseResume);
+document.getElementById('nextButton').addEventListener('click', showNextGraph);
+
+intervalId = setInterval(showNextGraph, 5000);  // Démarrez l'intervalle initial
